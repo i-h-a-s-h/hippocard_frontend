528630b13859acb46a6256308b4cb9435bc61f4f
Harikrishna <hk9117430@gmail.com>
2025-02-27 22:57:03 +0530

    minor fixes

--- hippocard/components/PageTransition.tsx
+++ hippocard/components/PageTransition.tsx
@@ -11,14 +11,16 @@ export default function PageTransition({ children }: PageTransitionProps) {
   const pathname = usePathname();
 
   return (
-    <motion.main
-      key={pathname}
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: 20 }}
-      transition={{ duration: 0.3, ease: "easeInOut" }}
-    >
-      {children}
-    </motion.main>
+    <AnimatePresence mode='wait'>
+      <motion.div
+        key={pathname}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 20 }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
   );
 } 
